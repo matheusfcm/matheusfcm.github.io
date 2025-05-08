27c7ae8c65b741d5dcc9f6ef95ac15d6a5fdf1bd
Matheus Motta <300079643@fvtc.edu>
2025-05-08 01:28:32 -0500

    Added carousel

--- site.js
+++ site.js
@@ -21,3 +21,40 @@ const welcomeDiv = document.getElementById("welcome");
 if (welcomeDiv) {
     welcomeDiv.textContent = message;
 }
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url });
+
+const images = document.querySelectorAll('#carousel img');
+let currentImage = 0;
+
+const showImages = () => {
+    const offset = currentImage % urls.length;
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length;
+        image.src = urls[imageIndex];
+    });
+};
+
+showImages();
+
+// handle button clicks
+document.getElementById('next').addEventListener('click', () => {
+    currentImage++;
+    showImages();
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentImage--;
+    showImages();
+});
+
+// auto-slide every 5 seconds
+setInterval(() => {
+    currentImage++;
+    showImages();
+}, 5000);
